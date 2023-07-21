681edf98e69839e9d1a95d9cf9a25d2bf291dbd8
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-07-21 07:25:08 +0200

    fix: hide container`s outline

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   position: relative;
-  outline: 1px solid tomato;
   margin: 0 auto;
   width: 100%;
   padding: 60px 0 0;
